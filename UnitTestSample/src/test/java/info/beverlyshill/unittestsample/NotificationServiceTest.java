378a7e36afd1d7f01624aa4af972f789323289f9
bhill2 <bjsrhill@gmail.com>
2013-03-03 13:52:49 -0500

    Added allSubscribedClientsReceibeMessage() and DRY refactoring
    Signed-off-by: bhill2 <bjsrhill@gmail.com>

--- UnitTestSample/src/test/java/info/beverlyshill/unittestsample/NotificationServiceTest.java
+++ UnitTestSample/src/test/java/info/beverlyshill/unittestsample/NotificationServiceTest.java
@@ -14,6 +14,10 @@ import org.mockito.Mockito;
 
 /**
  * Test class for NotificationService
+ * 
+ * Test that clients are allowed to subscribe to start receiving messages
+ * Test that subscribers allow to be unsubscribed to stop receiving messages
+ * Test that every time a new message comes it should be sent to all subscribers
  * @author bhill2
  */
 public class NotificationServiceTest {
@@ -21,12 +25,24 @@ public class NotificationServiceTest {
 	private NotificationService notificationService = new NotificationService();
 	
 	//Client
-	Client client = null;
+	private Client client = null;
+	
+	//Additional Client instances
+	private Client firstSubscriber = null;
+	private Client secondSubscriber = null;
+	
+	//Message
+	private Message message = null;
 	
 	@Before
 	public void setUp() throws Exception {
 		//Client mock
 		client = Mockito.mock(Client.class);
+		//Additional Client mocks
+		firstSubscriber = Mockito.mock(Client.class);
+		secondSubscriber = Mockito.mock(Client.class);
+		//Message mock
+		Message message = Mockito.mock(Message.class);
 	}
 
 	/**
@@ -35,7 +51,7 @@ public class NotificationServiceTest {
 	 */
 	@Test
 	public final void addedSubscriberSetsClient() {
-		//invoke test method
+		//invoke test method - client subscribes
 		notificationService.addSubscriber(client);
 		//assert that test method sets client name
 		assertNotNull("The value is null and should not be null.", notificationService.getClientName());
@@ -46,15 +62,26 @@ public class NotificationServiceTest {
 	 * Tests that the method invokes client.receive(message)
 	 */
 	@Test
-	public final void sendShouldSubscribedClientShouldReceiveMessage() {
-		//Message mock
-		Message message = Mockito.mock(Message.class);
-		//invoke method addSubscriber to set Client
+	public final void sendMessageClientShouldReceiveMessage() {
+		//invoke method addSubscriber - client subscribes
 		notificationService.addSubscriber(client);
 		//invoke method to test
 		notificationService.send(message);
 		//verify that subscribed client receives message
 		Mockito.verify(client).receive(message);
 	}
-
+	
+	/**
+	 * Test for send
+	 * Test that all subscribed clients receive
+	 * a message
+	 */
+	@Test
+	public final void allSubscribedClientsReceibeMessage() {
+		notificationService.addSubscriber(firstSubscriber);
+		notificationService.addSubscriber(secondSubscriber);
+		notificationService.send(message);
+		Mockito.verify(firstSubscriber).receive(message);
+		Mockito.verify(secondSubscriber).receive(message);
+	}
 }
