ec2382af1f68c88dd4ecf7a8cf4fd5f5488f8984
bhill2 <bjsrhill@gmail.com>
2013-03-03 13:51:54 -0500

    Added ArrayList so that multiple Clients can receive a message
    Signed-off-by: bhill2 <bjsrhill@gmail.com>

--- UnitTestSample/src/main/java/info/beverlyshill/unittestsample/NotificationService.java
+++ UnitTestSample/src/main/java/info/beverlyshill/unittestsample/NotificationService.java
@@ -3,6 +3,9 @@
  */
 package info.beverlyshill.unittestsample;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
@@ -10,22 +13,28 @@ import org.springframework.stereotype.Service;
 
 /**
  * Service that manages scribing and notifying clients
- * 
+ * Clients are allowed to subscribe to start receiving messages
+ * Subscribers allow to be unsubscribed to stop receiving messages
+ * Every time a new message comes it should be sent to all subscribers
  * @author bhill2
  */
 @Component
 public class NotificationService implements Message, Client {
 	
 	private Client client;
+	
+	private Collection<Client> allClients = new ArrayList<Client>();
 
 	public void addSubscriber(Client client) {
+		allClients.add(client);
 		this.client = client;
-		setClientName(this.client);
 		
 	}
 
 	public void send(Message message) {
-		this.receive(message);	
+		for(Client client : allClients) {
+			client.receive(message);
+		}
 	}
 	
 	public void setClientName(Client client) {
